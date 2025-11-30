73a1d5ea4f716e8e23fd655b77b01c54bb960e54
G7Shadow <l.jeremy.822001@gmail.com>
2025-11-30 06:11:26 -0500

    Just some updates

--- flake.nix
+++ flake.nix
@@ -2,35 +2,31 @@
   description = "My Nixos Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nix-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , home-manager
-    , ...
-    } @ inputs: {
-      nixosConfigurations.Alpha = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.jeremyl = import ./home.nix;
-
-            # Optional: Backup existing files
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.Alpha = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jeremyl = import ./home.nix;
+          home-manager.backupFileExtension = "backup";
+        }
+      ];
     };
+  };
 }
