e2e59e7457819792470bdbb9c6c61267597ac4e2
G7Shadow <l.jeremy.822001@gmail.com>
2026-01-27 20:57:18 -0500

    some fixes

--- flake-parts/default.nix
+++ flake-parts/default.nix
@@ -1,13 +1,13 @@
 {inputs, ...}: {
   systems = ["x86_64-linux"];
-  
+
   flake.nixosConfigurations.Omega = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {inherit inputs;};
-    
+
     modules = [
       ../modules/system/configuration.nix
-      
+
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager = {
@@ -18,7 +18,7 @@
           users.jeremyl = import ../modules/home/home.nix;
         };
       }
-      
+
       inputs.nix-index-database.nixosModules.nix-index
       {
         programs.nix-index-database.comma.enable = true;
